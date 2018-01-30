2d55b9c93ffe36bee3d682cc86fdfae217ba9052
KumarSathishAP <kumarsathishap@outlook.com>
2018-01-31 00:33:52 +0530

    first git commit for my selenium code

---
+++ src/test/java/com/qa/tests/SampleTest.java
@@ -0,0 +1,14 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void CheckingGitHub()
+	{
+		System.out.println("Working as Expected");
+		
+	}
+
+}
